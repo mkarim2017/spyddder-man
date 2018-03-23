94e8341c4da3483e989b108d55f235e19cca7cc9
gmanipon <gmanipon@jpl.nasa.gov>
2018-03-23 04:09:02 +0000

    base implementation

--- util.py
+++ util.py
@@ -24,7 +24,7 @@ def resolve_source(ctx_file):
     queue = "factotum-job_worker-scihub_throttled"
 
     # prod date
-    prod_date = time.stftime('%Y-%m-%d')
+    prod_date = time.strftime('%Y-%m-%d')
     
     return ( queue,
              best_url,
@@ -41,38 +41,30 @@ def extract_job(queue, localize_url, file, prod_name, prod_date, wuid=None, job_
         raise RuntimeError("Need to specify workunit id and job num.")
 
     # set job type and disk space reqs
-    job_type = "generate_l0b_lr_raw:%s" % job_version
+    job_version = "dev"
+    job_type = "job-spyddder-extract:{}".format(job_version)
     disk_usage = "50GB"
 
-    # set job queue based on project
-    project = "swot"
-
     # set localize urls
-    localize_urls = []
-    if l0a_lr_prime0 is not None:
-        localize_urls.append({'url': os.path.join(l0a_lr_prime0_url, "%s.tif" % l0a_lr_prime0) })
-        localize_urls.append({'url': os.path.join(l0a_lr_prime0_url, "%s.met.json" % l0a_lr_prime0)})
-    if l0a_lr_prime1 is not None:
-        localize_urls.append({'url': os.path.join(l0a_lr_prime1_url, "%s.tif" % l0a_lr_prime1) })
-        localize_urls.append({'url': os.path.join(l0a_lr_prime1_url, "%s.met.json" % l0a_lr_prime1)})
-    if l0a_lr_prime2 is not None:
-        localize_urls.append({'url': os.path.join(l0a_lr_prime2_url, "%s.tif" % l0a_lr_prime2) })
-        localize_urls.append({'url': os.path.join(l0a_lr_prime2_url, "%s.met.json" % l0a_lr_prime2)})
+    localize_urls = [{
+        "url": localize_url,
+        "local_path": file,
+    }]
 
     # get container mappings and dependency images from job spec
-    job_spec_file = os.path.abspath(os.path.join(BASE_PATH, '..', 'docker', 'job-spec.json.PGE_L0B_LR_RAW'))
+    job_spec_file = os.path.abspath(os.path.join(BASE_PATH, 'docker', 'job-spec.json.sling_extract_acquisition'))
     with open(job_spec_file) as f:
         job_spec = json.load(f)
     container_mappings = job_spec.get('imported_worker_files', {})
     dependency_images = job_spec.get('dependency_images', [])
 
     return {
-        "job_name": "%s-%s" % (job_type, l0b_lr_raw_id),
+        "job_name": "%s-%s" % (job_type, prod_name),
         "job_type": "job:%s" % job_type,
         "job_queue": queue,
         "container_mappings": container_mappings,
-        "soft_time_limit": 86400,
-        "time_limit": 86700,
+        "soft_time_limit": 14400,
+        "time_limit": 14700,
         "payload": {
             # sciflo tracking info
             "_sciflo_wuid": wuid,
@@ -84,15 +76,12 @@ def extract_job(queue, localize_url, file, prod_name, prod_date, wuid=None, job_
             },
   
             # job params
-            "l0a_lr_prime0": l0a_lr_prime0,
-            "l0a_lr_prime1": l0a_lr_prime1,
-            "l0a_lr_prime2": l0a_lr_prime2,
-            "l0b_lr_raw_id": l0b_lr_raw_id,
-            "ancillary1": ancillary1,
-            "ancillary2": ancillary2,
+            "file": file,
+            "prod_name": prod_name,
+            "prod_date": prod_date,
 
             # v2 cmd
-            "_command": "/home/ops/verdi/ops/swot-pcm/PGE_L0B_LR_RAW/run_pcm_int.sh",
+            "_command": "/home/ops/verdi/ops/spyddder-man/extract.py",
 
             # disk usage
             "_disk_usage": disk_usage,
